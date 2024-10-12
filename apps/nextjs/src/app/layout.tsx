254357f8723fb2c3c74762a6e8a435b8fc280327
Nauris Linde <naurislinde@gmail.com>
2024-10-12 20:33:49 +0300

    feat: added vercel analytics and speed-insights

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
@@ -81,6 +83,8 @@ export default function RootLayout(
           </main>
           <Toaster />
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
