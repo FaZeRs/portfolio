2962928f7272d267615c5e5c65fdd55b436f5b3d
Nauris Linde <naurislinde@gmail.com>
2024-10-14 21:37:53 +0300

    feat: added skills section

---
+++ apps/nextjs/src/app/_components/skills.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+
+import SectionHeading from "./section-heading";
+
+const fadeInAnimationVariants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+const skillsData = [
+  "C++",
+  "Qt",
+  "QML",
+  "CMake",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "PHP",
+  "Laravel",
+  "Docker",
+];
+
+const SkillSection = () => {
+  return (
+    <div>
+      <SectionHeading>My Skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="cursor-default rounded-xl border bg-white px-5 py-3 ease-in-out hover:border-zinc-700 dark:bg-white/10 dark:text-white/80"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillSection;
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { HydrateClient } from "~/trpc/server";
 import Intro from "./_components/intro";
+import SkillSection from "./_components/skills";
 
 export const runtime = "edge";
 
@@ -7,6 +8,9 @@ export default function HomePage() {
   return (
     <HydrateClient>
       <Intro />
+      <div className="mb-20 flex flex-col items-center space-y-40">
+        <SkillSection />
+      </div>
     </HydrateClient>
   );
 }
