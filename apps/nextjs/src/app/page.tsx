580baa62858ff4831c1105438f408c9071d977fd
Nauris Linde <naurislinde@gmail.com>
2024-10-15 20:00:29 +0300

    fix: removed trpc hydrate client from home page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,20 +1,17 @@
-import { HydrateClient } from "~/trpc/server";
 import Contact from "./_components/contact";
 import ExperienceSection from "./_components/experience";
 import Intro from "./_components/intro";
 import SkillSection from "./_components/skills";
 
-export const runtime = "edge";
-
 export default function HomePage() {
   return (
-    <HydrateClient>
+    <>
       <Intro />
       <div className="mb-20 flex flex-col items-center space-y-40">
         <SkillSection />
         <ExperienceSection />
         <Contact />
       </div>
-    </HydrateClient>
+    </>
   );
 }
