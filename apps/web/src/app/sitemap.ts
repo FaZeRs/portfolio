4c815700774f42522d2e1f005694d4c0f706687d
Nauris Linde <naurislinde@gmail.com>
2024-10-17 21:26:59 +0300

    fix: optimize sitemap generation

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -4,13 +4,14 @@ import { getBaseUrl } from "~/constants";
 import { projectsData } from "~/constants/projects-data";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = getBaseUrl();
   const routes: MetadataRoute.Sitemap = [
     "",
     "/about",
     "/projects",
     ...projectsData.map((project) => `/projects/${project.slug}`),
   ].map((route) => ({
-    url: `${getBaseUrl()}${route}`,
+    url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
