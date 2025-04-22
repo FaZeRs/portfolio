1a70c0704f47b3d123f330f70bdc9c046a71c636
Nauris Linde <naurislinde@gmail.com>
2025-04-22 23:01:50 +0300

    style: code smell fix

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,5 +4,5 @@ import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) throw new Error("Missing DATABASE_URL");
 
-const driver = neon(process.env.DATABASE_URL!);
+const driver = neon(process.env.DATABASE_URL);
 export const db = drizzle({ client: driver, schema, casing: "snake_case" });
