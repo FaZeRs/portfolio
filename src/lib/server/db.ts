e5c2f7f3873052b3ac7e634088cdff13a45f4776
Nauris Linde <naurislinde@gmail.com>
2025-04-22 21:44:09 +0300

    fix: changed to neon postgres client

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,7 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const driver = postgres(process.env.DATABASE_URL as string);
+const driver = neon(process.env.DATABASE_URL as string);
 export const db = drizzle({ client: driver, schema, casing: "snake_case" });
