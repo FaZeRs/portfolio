d9a9df5f8cc69efa0c662b4dfe84efd89148f37e
Nauris Linde <naurislinde@gmail.com>
2025-08-12 22:33:35 +0300

    fix: removed unused icon

--- src/components/users/actions.tsx
+++ src/components/users/actions.tsx
@@ -1,4 +1,4 @@
-import { MoreHorizontal, ReceiptTurkishLiraIcon } from "lucide-react";
+import { MoreHorizontal } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 import {
