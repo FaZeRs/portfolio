dbe4749441c4b09c786d426011fe4d9640d14608
Nauris Linde <naurislinde@gmail.com>
2025-09-18 22:25:44 +0300

    Guestbook (#23)
    * feat: guestbook
    
    * fix: code review fixes
    
    * fix: type errors
    
    * fix: correct import name and improve accessibility attributes

--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "~/trpc/init";
 import { blogRouter } from "~/trpc/router/blog";
 import { commentRouter } from "~/trpc/router/comment";
 import { experienceRouter } from "~/trpc/router/experience";
+import { guestbookRouter } from "~/trpc/router/guestbook";
 import { projectRouter } from "~/trpc/router/project";
 import { snippetRouter } from "~/trpc/router/snippet";
 import { statsRouter } from "~/trpc/router/stats";
@@ -15,6 +16,7 @@ export const appRouter = createTRPCRouter({
   blog: blogRouter,
   comment: commentRouter,
   stats: statsRouter,
+  guestbook: guestbookRouter,
 });
 
 export type AppRouter = typeof appRouter;
