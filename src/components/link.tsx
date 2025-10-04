65dccd29f7769748501db482d2c7106eeade48b8
Nauris <naurislinde@gmail.com>
2025-10-04 22:11:16 +0300

    fix: add rel attribute to TanstackLink for security

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -13,7 +13,7 @@ export default function Link({
   icon,
 }: Readonly<ProjectLinksProps>) {
   return (
-    <TanstackLink target="_blank" to={url}>
+    <TanstackLink rel="noopener noreferrer" target="_blank" to={url}>
       <div className="flex cursor-pointer items-center gap-x-2 rounded-md border border-transparent bg-white px-4 py-2 text-zinc-700 duration-200 hover:border-zinc-200 dark:bg-white/10 dark:text-white hover:dark:border-zinc-700">
         {icon}
         {title}
