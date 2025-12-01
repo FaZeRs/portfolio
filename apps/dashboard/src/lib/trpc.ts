289ad2deefe0f6fc2e7d9960cb7a5e1e75e9243b
Nauris Linde <naurislinde@gmail.com>
2025-12-01 23:57:39 +0200

    fix: revert trpc changes for router

--- apps/dashboard/src/lib/trpc.ts
+++ apps/dashboard/src/lib/trpc.ts
@@ -1,6 +1,54 @@
 // biome-ignore lint/performance/noNamespaceImport: valid case
 import * as Api from "@acme/api";
-
+import { createIsomorphicFn } from "@tanstack/react-start";
+import { getRequestHeaders } from "@tanstack/react-start/server";
+import {
+  createTRPCClient,
+  httpBatchStreamLink,
+  loggerLink,
+  unstable_localLink,
+} from "@trpc/client";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
+import SuperJSON from "superjson";
+import { auth } from "./auth/server";
+import { env } from "./env/server";
+import { getBaseUrl } from "./utils";
+
+export const makeTRPCClient = createIsomorphicFn()
+  .server(() =>
+    createTRPCClient<Api.AppRouter>({
+      links: [
+        unstable_localLink({
+          router: Api.appRouter,
+          transformer: SuperJSON,
+          createContext: () => {
+            const headers = new Headers(getRequestHeaders());
+            headers.set("x-trpc-source", "tanstack-start-server");
+            return Api.createTRPCContext({ auth, headers });
+          },
+        }),
+      ],
+    })
+  )
+  .client(() =>
+    createTRPCClient<Api.AppRouter>({
+      links: [
+        loggerLink({
+          enabled: (op) =>
+            env.NODE_ENV === "development" ||
+            (op.direction === "down" && op.result instanceof Error),
+        }),
+        httpBatchStreamLink({
+          transformer: SuperJSON,
+          url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            const headers = new Headers();
+            headers.set("x-trpc-source", "tanstack-start-client");
+            return headers;
+          },
+        }),
+      ],
+    })
+  );
 
 export const { useTRPC, TRPCProvider } = createTRPCContext<Api.AppRouter>();
--- apps/dashboard/src/router.tsx
+++ apps/dashboard/src/router.tsx
@@ -1,18 +1,8 @@
 import { createRouter } from "@acme/shared/create-router";
-import { createTRPC } from "@acme/shared/create-trpc";
-import { auth } from "~/lib/auth/server";
-import { env } from "~/lib/env/server";
-import { TRPCProvider } from "~/lib/trpc";
-import { getBaseUrl } from "~/lib/utils";
+import { makeTRPCClient, TRPCProvider } from "~/lib/trpc";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  const makeTRPCClient = createTRPC({
-    sourceName: "dashboard",
-    auth,
-    env,
-    baseUrl: getBaseUrl(),
-  });
   return createRouter<typeof routeTree>({
     routeTree,
     makeTRPCClient,
